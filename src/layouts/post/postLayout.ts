9656862f95faa44fb67c5554a5e70ea196152fe0
kamil <frankiewiczkamil@gmail.com>
2022-12-29 23:59:24 +0100

    filter out draft posts :sparkles:

--- src/layouts/post/postLayout.ts
+++ src/layouts/post/postLayout.ts
@@ -1,4 +1,7 @@
+import type { MarkdownInstance } from "astro";
+
 type DateInfo = { editedAt?: string, publishedAt?: string }
+type Post = MarkdownInstance<Record<string, any>>;
 
 const selectJoiner = ({ publishedAt }: DateInfo): string => publishedAt ? ', ' : '';
 
@@ -11,3 +14,9 @@ const selectPublishedAtText = ({ publishedAt }: DateInfo): string => publishedAt
 
 export const selectPostDate = (frontmatter: DateInfo): string =>
     selectPublishedAtText(frontmatter) + selectEditedAtText(frontmatter);
+
+export const selectPublishedAtTimestamp = (post: Post) => new Date(post.frontmatter.publishedAt).getTime();
+
+export const isNotDraft = (post: Post) => !post.frontmatter.draft;
+
+export const sortByPublishedAtDesc = (p1: Post, p2: Post) => selectPublishedAtTimestamp(p2) - selectPublishedAtTimestamp(p1);  
