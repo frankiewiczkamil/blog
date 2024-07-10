39961a31a36658b46b326b260113f8ad72cd6dad
kamil <frankiewiczkamil@gmail.com>
2024-07-10 21:00:27 +0200

    Add translations in navbar :globe_with_meridians:

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+    en: 'english',
+    pl: 'polish',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+    en: {
+        'nav.home': 'Home',
+        'nav.about': 'About',
+        'nav.blog': 'Blog',
+    },
+    pl: {
+        'nav.home': 'Start',
+        'nav.about': 'O mnie',
+        'nav.blog': 'Blog',
+    },
+} as const;
