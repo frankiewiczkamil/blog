859caf2222797ffe4df49fa30742687cbf5acfb2
kamil <frankiewiczkamil@gmail.com>
2024-07-24 21:11:14 +0200

    Add libraries for icons handling :heavy_plus_sign: :wrench:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: "en",
-        locales: ["pl", "en"],
-        routing: {
-            prefixDefaultLocale: false,
-        }
-    }
-})
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['pl', 'en'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  integrations: [icon()],
+});
