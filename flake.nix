eaf007b1c682c1d35dbcb60769c7745124e4de02
kamil <frankiewiczkamil@gmail.com>
2024-11-11 00:54:20 +0100

    Format `flake` file :recycle:

--- flake.nix
+++ flake.nix
@@ -4,24 +4,27 @@
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.*.tar.gz";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       allSystems = [
         "aarch64-darwin" # 64-bit ARM macOS
       ];
 
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
+      forAllSystems =
+        f: nixpkgs.lib.genAttrs allSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
     in
     {
-      devShells = forAllSystems ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            nodejs
-            pnpm
-          ];
-        };
-      });
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs
+              pnpm
+            ];
+          };
+        }
+      );
     };
 }
